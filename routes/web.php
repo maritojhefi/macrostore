98a484c5ab48530f75f082eeef5b6cdff8582899
unknown <maritojhefi@gmail.com>
2022-09-23 14:47:04 -0400

    listo

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Elastic\Elasticsearch\ClientBuilder;
+use Elastic\Elasticsearch\Exception\ClientResponseException;
+use Elastic\Elasticsearch\Exception\ServerResponseException;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,11 +31,22 @@ Route::get('/admin1', function () {
     $client = ClientBuilder::create()
         ->setHosts(['localhost:9200'])
         ->build();
+    $params = [
+        'index' => 'users',
+        'body'  => ['users' => 'marie']
+    ];
 
-    // Info API
-    $response = $client->info();
+    try {
+        $response = $client->index($params);
+    } catch (ClientResponseException $e) {
+        // manage the 4xx error
+    } catch (ServerResponseException $e) {
+        // manage the 5xx error
+    } catch (Exception $e) {
+        // eg. network error like NoNodeAvailableException
+    }
 
-    echo $response['version']['number']; // 8.0.0
+    print_r($response->asArray());  // response body content as array
     return view('admin.prueba.prueba');
 });
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
