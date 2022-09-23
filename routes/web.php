546ede35553f91d741940ef03735e049b0c3d1ae
unknown <maritojhefi@gmail.com>
2022-09-23 15:20:13 -0400

    listo

--- routes/web.php
+++ routes/web.php
@@ -29,48 +29,48 @@ Route::get('/dashboard', function () {
 Auth::routes();
 Route::get('/admin1', function () {
 
-    $client = ClientBuilder::create()
-    ->setHosts(['localhost:9200'])
-    ->build();
+    // $client = ClientBuilder::create()
+    //     ->setHosts(['localhost:9200'])
+    //     ->build();
 
-    
+    // $response = $client->info();
 
+    // echo $response['version']['number']; // 8.0.0
+    // $client = ClientBuilder::create()
+    //     ->setHosts(['localhost:9200'])
+    //     ->build();
+    // $params = [
+    //     'index' => 'users',
+    //     'body'  => ['users' => 'mario']
+    // ];
 
-    $client = ClientBuilder::create()
-        ->setHosts(['localhost:9200'])
-        ->build();
-    $params = [
-        'index' => 'users',
-        'body'  => ['users' => 'mario']
-    ];
+    // try {
+    //     $response = $client->index($params);
+    // } catch (ClientResponseException $e) {
+    //     // manage the 4xx error
+    // } catch (ServerResponseException $e) {
+    //     // manage the 5xx error
+    // } catch (Exception $e) {
+    //     // eg. network error like NoNodeAvailableException
+    // }
 
-    try {
-        $response = $client->index($params);
-    } catch (ClientResponseException $e) {
-        // manage the 4xx error
-    } catch (ServerResponseException $e) {
-        // manage the 5xx error
-    } catch (Exception $e) {
-        // eg. network error like NoNodeAvailableException
-    }
+    // $params = [
+    //     'index' => 'users',
+    //     'body'  => [
+    //         'query' => [
+    //             'match' => [
+    //                 'users' => 'mare'
+    //             ]
+    //         ]
+    //     ]
+    // ];
+    // $response = $client->search($params);
+    // dd($response['hits']);
+    // printf("Total docs: %d\n", $response['hits']['total']['value']);
+    // printf("Max score : %.4f\n", $response['hits']['max_score']);
+    // printf("Took      : %d ms\n", $response['took']);
 
-    $params = [
-        'index' => 'users',
-        'body'  => [
-            'query' => [
-                'match' => [
-                    'users' => 'mare'
-                ]
-            ]
-        ]
-    ];
-    $response = $client->search($params);
-    dd($response['hits']);
-    printf("Total docs: %d\n", $response['hits']['total']['value']);
-    printf("Max score : %.4f\n", $response['hits']['max_score']);
-    printf("Took      : %d ms\n", $response['took']);
-    
-    print_r($response['hits']['hits']); // documents
-    //return view('admin.prueba.prueba');
+    // print_r($response['hits']['hits']); // documents
+    return view('admin.prueba.prueba');
 });
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
